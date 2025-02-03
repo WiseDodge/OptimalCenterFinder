ca6e7f8f99e5e50ca16683d276473b51b4e6f6c3
WiseDodge <wisedodgecoding@gmail.com>
2025-02-03 15:10:32 -0500

    v1.2; input handler

--- OptimalCenterFinder.py
+++ OptimalCenterFinder.py
@@ -1,26 +1,51 @@
 def calculate_square_corners(center, map_size):
     cx, cy, cz = center
-
-
-# odd/even -> for symmetry
     is_odd = map_size % 2 != 0
     offset = (map_size - 1) // 2 if is_odd else map_size // 2
     
-    corner1 = (cx - offset, cy, cz - offset)
-    corner2 = (cx + offset, cy, cz + offset)
+# converge result    corner1 = (cx - offset, cy, cz - offset)
+# converge result    corner2 = (cx + offset, cy, cz + offset)
 
 # returns calculated corners
-    return corner1, corner2
-
-# test
-center_block = (-530, 90, -145)
-map_size = 9
+    return cx - offset, cy, cz - offset, cx + offset, cy, cz + offset
 
 
-# Calculate the corners
-pos1, pos2 = calculate_square_corners(center_block, map_size)
+def get_center_input():
+    try:
+        coords = input("Enter the center block's coordinates (x y z): ")
+        if len(coords) != 3:
+            raise ValueError("Please provide exactly THREE coordinates.")
+        return tuple(map(int, coords))
+        
+    except ValueError as e:    
+        print(f"The input is invalid: {e}. Try again.")
+        return get_center_input()
 
 
-# output
-print("Pos1: ", pos1) # north
-print("Pos2: ", pos2) # south
+# map size
+def get_map_size():
+    try:
+        size = int(input("Enter the map size (example: 9, 300, etc): "))
+        if size <= 0:
+            raise ValueError("The given map size has to be a positive integer.")
+        return size
+    
+    except ValueError as e:
+        print(f"The input is invalid: {e}. Try again.")
+        return get_map_size()
+
+# center_block = (-530, 90, -145)
+# map_size = 9
+
+
+
+if __name__ == "__main__":
+    center_block = get_center_input()
+    map_size = get_map_size()
+
+    pos1, pos2  = calculate_square_corners(center_block, map_size)
+
+    # output
+    print("\nResults:")
+    print(f"Pos1: {pos1}") # north
+    print(f"Pos2: {pos2}") # south
